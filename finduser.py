d11b60992d9413189324b73bf7d157ed456e786a
grilo <joao.grilo@gmail.com>
2016-01-31 02:36:27 +0100

    Cleanup the process pool after we're done. Convert the date from openDate to POSIX/Epoch.

--- finduser.py
+++ finduser.py
@@ -5,6 +5,7 @@ import subprocess
 import shlex
 import json
 import multiprocessing
+import time
 
 
 class FindUser:
@@ -12,22 +13,38 @@ class FindUser:
         self.command = command
         self.workers = workers
 
+    def _openDate_to_epoch(self, ts):
+        # Also normalize the data being sent which always uniform
+        if "dayOfMonth" in ts.keys():
+            ts["day"] = ts["dayOfMonth"]
+        if "hourOfDay" in ts.keys():
+            ts["hour"] = ts["hourOfDay"]
+        if "second" not in ts.keys():
+            ts["second"] = "00"
+        struct_time = time.strptime(" ".join([ts["year"], ts["month"], ts["day"], \
+                        ts["hour"], ts["minute"], ts["second"]]), "%Y %m %d %H %M %S")
+        return time.mktime(struct_time)
+
     def find(self, uuid):
-        logging.debug("Querying for user %d" % (uuid))
+        logging.warn("Querying for user %d" % (uuid))
         cmd = " ".join([self.command, str(uuid)])
         stdout = subprocess.check_output(shlex.split(cmd), stderr= subprocess.STDOUT)
         data = json.loads(stdout.decode('utf8'))
-        return {
-            "cip": uuid,
-            "products": data,
-        }
+        # Transform the map inplace
+        for d in data:
+            d["user"] = uuid
+            d["openDate"] = self._openDate_to_epoch(d["openDate"])
+        return data
 
     def parallel_find(self, iterator):
         proc_pool = multiprocessing.Pool(self.workers)
-        return proc_pool.imap_unordered(self.find, iterator)
+        for i in proc_pool.imap_unordered(self.find, iterator):
+            yield i
+        proc_pool.close()
+        proc_pool.join()
+
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.DEBUG)
     f = FindUser("python simulate.py")
-    #print(f.find(1))
-    [x for x in f.parallel_find(range(100))]
+    #[x for x in f.parallel_find(range(100))]
