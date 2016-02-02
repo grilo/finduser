32ec85ffde8efdba8d7cd12a88d82d97d40a86c8
grilo <joao.grilo@gmail.com>
2016-02-02 02:57:08 +0100

    Add default query parameters, normally we don't want blocked users.

--- settings.py
+++ settings.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
+# This is what we assume by default the messages received are encoded in
+default_encoding = "utf8"
+
+# data.py
 # The name of the data file being used
 db_name = 'users.sqlite'
 # How many seconds until we may refresh dirty users
-dirty_user_refresh = 300
-# The executable that the batch job must run against
-find_user_exec = "python simulate.py"
-# This is what we assume by default the messages received are encoded in
-default_encoding = "utf8"
+db_dirty_user_refresh = 300
+# Default parameters which will be inserted automatically in the query
+# unless explicitly overriden by the user
+db_default_properties = { "accBlocked": "=0" }
 
 # web.py
 web_address = '0.0.0.0'
@@ -15,3 +18,5 @@ web_port = 8080
 
 # batch.py
 batch_default_workers = 16
+# The executable that the batch job must run against
+batch_find_user_exec = "python simulate.py"
