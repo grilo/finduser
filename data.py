70d14c03012544012f18471042842d5d4dbfe3a6
grilo <joao.grilo@gmail.com>
2016-02-03 02:15:54 +0100

    Minor refactor due to moving the product to the models file. Also start using the generic validator class, currently only the Product schema is being used. Use the new infrastructure to validate more stuff which was being ignored before. Teach the DAO to handle boolean values correctly.

--- data.py
+++ data.py
@@ -7,24 +7,31 @@ import time
 import extlibs.peewee as orm
 
 import models
-import product
 import settings
+import schema
+
+class DoesNotExist(orm.DoesNotExist):
+    pass
 
 
 class Access:
 
     def __init__(self):
+        self.product_validator = schema.Validator(models.Product.python_schema())
         models.db.connect()
-        models.db.create_tables([models.User, product.Product], True)
+        models.db.create_tables([models.User, models.Product], True)
 
     def get_product_fields(self):
-        return product.get_schema()
+        return models.Product.python_schema()
 
     def get_user_by_properties(self, properties):
         logging.debug("Get user by properties: %s" % (properties))
+        assert "product" in properties.keys()
 
         # Set default parameters
         for p in properties["product"]:
+            if not self.product_validator.partial(p):
+                raise AssertionError
             for k, v in settings.db_default_properties.items():
                 if k in p.keys(): continue
                 p[k] = v
@@ -35,19 +42,26 @@ class Access:
         # fetching the first user which matches all of the queries
         lists_of_users = []
         for p in properties["product"]:
-            print(p)
             clauses = []
             for k, v in p.items():
-                if v.startswith(">"):
-                    clauses.append((getattr(product.Product, k) > v.lstrip(">=")))
+                if type(v) == bool:
+                    clauses.append((getattr(models.Product, k) == v))
+                elif v.startswith(">"):
+                    clauses.append((getattr(models.Product, k) > v.lstrip(">=")))
                 elif v.startswith("<"):
-                    clauses.append((getattr(product.Product, k) <= v.lstrip("<=")))
+                    clauses.append((getattr(models.Product, k) <= v.lstrip("<=")))
                 else:
-                    clauses.append((getattr(product.Product, k) == v.lstrip("=")))
-            query = models.User.select().where(models.User.dirty == False).join(product.Product).where(functools.reduce(operator.and_, clauses))
+                    clauses.append((getattr(models.Product, k) == v.lstrip("=")))
+            query = models.User.select().where(models.User.dirty == False).join(models.Product).where(functools.reduce(operator.and_, clauses))
             lists_of_users.append(query)
 
         """
+        Of interest is noting that the '&' operator has different context:
+        in WHERE clauses it works like AND
+        If the left and right side are already built SQL queries, it works
+        as INTERSECT
+
+
         From peewee (orm) documentation:
 
         customers = Customer.select(Customer.city).where(Customer.state == 'KS')
@@ -57,20 +71,13 @@ class Access:
         cities = (customers & stores).order_by(SQL('city'))
         """
 
-        # Of interest is noting that the '&' operator has different context:
-        # in WHERE clauses it works like AND
-        # If the left and right side are already built SQL queries, it works
-        # as INTERSECT
-
         try:
             user = functools.reduce(operator.and_, lists_of_users).get()
             user.dirty = True
             user.save()
-            logging.info("Found user for query: %s" % (properties))
             return user.cip
         except orm.DoesNotExist:
-            logging.error("No user found for query: %s" % (properties))
-            return ''
+            raise LookupError
 
     def get_dirty_users(self):
         delta = time.time() - settings.db_dirty_user_refresh
@@ -82,14 +89,18 @@ class Access:
         # Ensure that the data is actually good for insertion
         validated = []
         for p in products:
-            if not product.validate(p):
+            if self.product_validator.full(p):
+                validated.append(p)
+            else:
                 logging.error("Ignoring invalid data (%s): %s" % (p["user"], p))
-                continue
-            validated.append(p)
+
+        if len(validated) <= 0:
+            logging.error("No valid entries found for the user found, refusing any updates.")
 
         logging.warning("Inserting products for: %s" % (str(products[0]["user"])))
         with models.db.atomic():
-            product.Product.insert_many(validated).upsert(upsert=True).execute()
+            q = models.Product.insert_many(validated).upsert(upsert=True)
+            q.execute()
 
         # Mark the user's data as fresh
         return models.User.update(dirty=False, lastUpdate=time.time()).where(models.User.cip == products[0]["user"]).execute()
