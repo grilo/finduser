831ba8acc8cd8abc6683e20f4a6c6766e48689c5
grilo <joao.grilo@gmail.com>
2016-02-04 03:32:44 +0100

    Isolate IO. Refactor for clarity.

--- finduser/data.py
+++ finduser/data.py
@@ -4,6 +4,8 @@ import logging
 import operator
 import functools
 import time
+import re
+
 import extlibs.peewee as orm
 
 import finduser.models
@@ -19,21 +21,66 @@ class Access:
         finduser.models.db.connect()
         finduser.models.db.create_tables([finduser.models.User, finduser.models.Product], True)
 
+    def _dbIO(self, query):
+        try:
+            return query.execute()
+        except peewee.OperationalError:
+            logging.critical("Unable to perform database update, probably due to locking: %s" % (query))
+            raise IOError
+
+    def _split_operator_value(self, expression):
+        # Don't do weird stuff for booleans
+        if type(expression) == bool:
+            return '', expression
+
+        # We have no match, this means there's no operator attached
+        m = re.search('([<=>]+)(.*)', expression)
+        if m is None:
+            return '', expression
+        return m.group(1), m.group(2)
+
     def get_product_fields(self):
         return finduser.models.Product.python_schema()
+
+    def get_product_query(self, raw_product):
+        clauses = []
+        product = raw_product.copy()
+
+        # Set default values
+        for k, v in self.default_properties["product"].items():
+            if not k in product.keys():
+                product[k] = v
+
+        for k, v in product.items():
+            try:
+                op, v = self._split_operator_value(v)
+            except TypeError:
+                logging.critical("Unable to split operator and value for: %s" % (v))
+                raise TypeError
+
+            if not self.product_validator.partial({k: v}):
+                raise AssertionError
+
+            if op.startswith(">"):
+                clauses.append((getattr(finduser.models.Product, k) >= v))
+            elif op.startswith("<"):
+                clauses.append((getattr(finduser.models.Product, k) <= v))
+            else:
+                clauses.append((getattr(finduser.models.Product, k) == v))
+
+        return finduser.models.User.select() \
+                .where(finduser.models.User.dirty == False) \
+                .join(finduser.models.Product) \
+                .where(functools.reduce(operator.and_, clauses))
+
 
     def get_user_by_properties(self, properties):
         logging.debug("Get user by properties: %s" % (properties))
         assert type(properties) == dict
 
-        # Set default parameters such as "accBlocked: False"
-        for p in properties["product"]:
-            if not self.product_validator.partial(p):
-                raise AssertionError
-            for k, v in self.default_properties["product"].items():
-                if k in p.keys(): continue
-                p[k] = v
-
+        lists_of_users = []
+        for product in properties["product"]:
+            lists_of_users.append(self.get_product_query(product))
 
         """
         For this to work, we need to create an intersection between
@@ -55,56 +102,47 @@ class Access:
         # Get all cities in kanasas where we have both customers and stores.
         cities = (customers & stores).order_by(SQL('city'))
         """
-        lists_of_users = []
-        for p in properties["product"]:
-            clauses = []
-            for k, v in p.items():
-                if type(v) == bool:
-                    clauses.append((getattr(finduser.models.Product, k) == v))
-                elif v.startswith(">"):
-                    clauses.append((getattr(finduser.models.Product, k) > v.lstrip(">=")))
-                elif v.startswith("<"):
-                    clauses.append((getattr(finduser.models.Product, k) <= v.lstrip("<=")))
-                else:
-                    clauses.append((getattr(finduser.models.Product, k) == v.lstrip("=")))
-            query = finduser.models.User.select().where(finduser.models.User.dirty == False).join(finduser.models.Product).where(functools.reduce(operator.and_, clauses))
-            lists_of_users.append(query)
 
         try:
-            user = functools.reduce(operator.and_, lists_of_users).get()
-            user.dirty = True
-            user.save()
-            return user.cip
+            user = self._dbIO(functools.reduce(operator.and_, lists_of_users).limit(1)).next()
+            self.refresh_user(user.personId, time.time())
+            return user.personId
         except orm.DoesNotExist:
             raise LookupError
 
     def get_dirty_users(self):
         delta = time.time() - self.dirty_user_refresh
-        return [u.cip for u in finduser.models.User.select(finduser.models.User.cip).where(finduser.models.User.dirty == True).where(finduser.models.User.lastUpdate < delta).limit(10000)]
+        query = finduser.models.User.select(finduser.models.User.personId) \
+                    .where(finduser.models.User.dirty == True) \
+                    .where(finduser.models.User.lastUpdate < delta) \
+                    .limit(10000)
+        return [u.personId for u in query]
 
-    def update_products(self, products):
-        if len(products) <= 0: return
+    def update_products(self, personId, products):
+        if len(products) <= 0:
+            # Either the user doesn't exist or it has no products,
+            # we mark it as fresh and return
+            return self._dbIO(finduser.models.User.update(dirty=False, lastUpdate=time.time()) \
+                        .where(finduser.models.User.personId == personId))
 
         # Ensure that the data is actually good for insertion
-        validated = []
-        for p in products:
-            if self.product_validator.full(p):
-                validated.append(p)
-            else:
-                logging.error("Ignoring invalid data (%s): %s" % (p["user"], p))
-
-        if len(validated) <= 0:
-            logging.error("No valid entries found for the user found, refusing any updates.")
+        valid_products = [p for p in products if self.product_validator.full(p)]
+        if len(valid_products) <= 0:
+            logging.error("No valid entries found for personId (%s), refusing updates." % (personId))
+            return
 
         with finduser.models.db.atomic():
-            logging.warning("Inserting products for: %s" % (str(products[0]["user"])))
-            q = finduser.models.Product.insert_many(validated).upsert(upsert=True)
-            q.execute()
+            logging.warning("Inserting products for: %s" % (personId))
+            q = finduser.models.Product.insert_many(valid_products).upsert(upsert=True)
+            self._dbIO(q)
 
         # Mark the user's data as fresh
-        return finduser.models.User.update(dirty=False, lastUpdate=time.time()).where(finduser.models.User.cip == products[0]["user"]).execute()
+        return self._dbIO(finduser.models.User.update(dirty=False, lastUpdate=time.time()) \
+                    .where(finduser.models.User.personId== personId))
 
 
-    def touch_user(self, properties):
+    def refresh_user(self, personId, lastUpdate=0.0):
+        properties = {"personId": personId, "dirty": True, "lastUpdate": lastUpdate}
+
         with finduser.models.db.atomic():
-            finduser.models.User.insert(**properties).upsert(upsert=True).execute()
+            self._dbIO(finduser.models.User.insert(**properties).upsert(upsert=True))
