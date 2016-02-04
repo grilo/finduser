6d03d0aeda71674313fe6162493b988e7df02b40
grilo <joao.grilo@gmail.com>
2016-02-04 03:31:23 +0100

    Revert schema naming, YAGNI. Add log options.

--- settings.py
+++ settings.py
@@ -2,10 +2,12 @@
 
 # This is what we assume by default the messages received are encoded in
 default_encoding = "utf8"
+log_format = '%(asctime)s::%(levelname)s::%(message)s'
+log_level = 'INFO'
 
 # data.py
 # The name of the data file being used
-db_name = 'userschema'
+db_name = 'users.sqlite'
 # How many seconds until we may refresh dirty users
 db_dirty_user_refresh = 300
 # Default parameters which will be inserted automatically in the query
