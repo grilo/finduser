2716a5b5b0efec6d52a5468942724ceb39b341d2
grilo <joao.grilo@gmail.com>
2016-02-04 01:01:35 +0100

    Make pylint happier.

--- batch.py
+++ batch.py
@@ -1,23 +1,41 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Find user utility.
+
+
+The goal is to run multiple command line utilities and insert them into a
+RDBMS. This will allow us to find users with specific characteristics as
+test case data based on "real" (ciphered/masked) information.
+
+Example:
+    All of the command line parameters are optional, defaults are retrieved
+    from settings.py
+
+        $ python batch.py -w 16 -x 'java -jar finduser.jar' -v
+
+    https://github.com/grilo/finduser
+"""
 
 import logging
 logging.basicConfig(format='%(asctime)s::%(levelname)s::%(message)s')
 import argparse
-import sys
 import time
 
-import finduser.finduser
+import finduser.product
 import finduser.data
 import settings
 
 
-if __name__ == '__main__':
+def main():
+    """Main method."""
 
-    parser = argparse.ArgumentParser(description='Execute a command line utility, parse its output and insert it into a database.')
+    desc = 'Execute several utilities and insert the output into a database.'
+    parser = argparse.ArgumentParser(description=desc)
 
     parser.add_argument("-v", "--verbose", action="store_true", \
         help="Increase output verbosity")
-    parser.add_argument("-w", "--workers", type=int, default=settings.batch_default_workers, \
+    parser.add_argument("-w", "--workers", type=int, \
+        default=settings.batch_default_workers, \
         help="Amount of subprocesses to launch which will execute the script")
     parser.add_argument("-x", "--execute", default=settings.batch_find_user_exec, \
         help="The utility to be executed (fixed parameters may be specified).")
@@ -28,14 +46,19 @@ if __name__ == '__main__':
     if args.verbose:
         logging.getLogger().setLevel(logging.DEBUG)
 
-    f = finduser.finduser.FindUser(args.execute, args.workers, settings.default_encoding)
+    gtm_product = finduser.product.GTM(args.execute, args.workers, settings.default_encoding)
     dao = finduser.data.Access(settings.db_default_properties, settings.db_dirty_user_refresh)
 
     while True:
         # Get the list of dirty users which represent users with tainted
         # or outdated info, and freshen their data by querying GTM directly
-        for products in f.parallel_find(dao.get_dirty_users()):
+        for products in gtm_product.parallel_find(dao.get_dirty_users()):
             dao.update_products(products)
 
         logging.info("All users processed, taking a nap...")
         time.sleep(5)
+
+
+if __name__ == '__main__':
+    main()
+
--- web.py
+++ web.py
@@ -7,62 +7,64 @@ import extlibs.bottle as bottle
 import finduser.data
 import settings
 
-app = bottle.Bottle()
-dao = data.Access()
+APP = bottle.Bottle()
+DAO = finduser.data.Access(settings.db_default_properties, settings.db_dirty_user_refresh)
 
-@app.hook('after_request')
+@APP.hook('after_request')
 def enable_cors():
     """
     You need to add some headers to each request.
     Don't use the wildcard '*' for Access-Control-Allow-Origin in production.
     """
     bottle.response.headers['Access-Control-Allow-Origin'] = '*'
-    bottle.response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
-    bottle.response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+    bottle.response.headers['Access-Control-Allow-Methods'] = \
+        'PUT, GET, POST, DELETE, OPTIONS'
+    bottle.response.headers['Access-Control-Allow-Headers'] = \
+        'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
 
-@app.route('/<filename:re:.*\.(css|js|jpg|png|gif|ico|ttf|eot|woff|woff2|svg|jsr|html)>')
+@APP.route('/<filename:re:.*\.(css|js|jpg|png|gif|ico|ttf|eot|woff|woff2|svg|jsr|html)>')
 def static_files(filename):
     return bottle.static_file(filename, root='static/')
 
-@app.route('/')
+@APP.route('/')
 def hello():
     return bottle.static_file('index.html', root='static/')
 
-"""
-    Enforce the database the update the information for user <cip>
-"""
-@app.route('/user/<cip>', method=['POST'])
+@APP.route('/user/<cip>', method=['POST'])
 def touch_user(cip):
-    dao.touch_user({"cip": cip, "dirty": True, "lastUpdate": 0.0})
+    """Enforce the database the update the information for user <cip>."""
+    DAO.touch_user({"cip": cip, "dirty": True, "lastUpdate": 0.0})
 
-"""
-    Model to build the user interfaces
-"""
-@app.route('/finduser/model')
+@APP.route('/finduser/model')
 def user_model():
-    model = { "product": {} }
-    for field_name, field_type in dao.get_product_fields().items():
+    """Model to build the user interfaces."""
+    model = {
+        "product": {}
+    }
+    for field_name, field_type in DAO.get_product_fields().items():
         model["product"][field_name] = str(field_type.__name__)
     return json.dumps(model)
 
-@app.route('/finduser', method=['POST'])
+@APP.route('/finduser', method=['POST'])
 def get_users():
     request = bottle.request.body.read().decode(settings.default_encoding)
     logging.info("Find user: %s" % (request))
     try:
         json_obj = json.loads(request)
     except json.decoder.JSONDecodeError:
-        return bottle.abort(400, "I'm unable to parse the JSON string sent: %s" % (request))
+        return bottle.abort(400, \
+            "I'm unable to parse the JSON string sent: %s" % (request))
 
     try:
-        cip = dao.get_user_by_properties(json_obj)
+        cip = DAO.get_user_by_properties(json_obj)
         return json.dumps(cip)
     except AssertionError:
-        return bottle.abort(400, "JSON being sent doesn't match the expected schema: %s" % (request))
+        return bottle.abort(400, \
+            "JSON being sent doesn't match the expected schema: %s" % (request))
     except LookupError:
         return bottle.abort(404, "Unable to find a user matching the criteriae: %s." % (request))
 
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.DEBUG)
-    bottle.run(app, host=settings.web_address, port=settings.web_port, debug=True)
+    bottle.run(APP, host=settings.web_address, port=settings.web_port, debug=True)
