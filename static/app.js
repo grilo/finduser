fe93abd680cb84a4772bf199aebc7e72e60bf92d
grilo <joao.grilo@gmail.com>
2016-02-09 00:14:08 +0100

    Refactor the UI to be dynamically generated. We now ask the plugin manager for whatever models exist for each of the installed plugins, and generate the query forms based on that information.

--- static/app.js
+++ static/app.js
@@ -1,51 +1,51 @@
-function addProduct (e) {
+function addModel (e) {
     e.preventDefault();
 
-    getDataModel().success(function (response) {
-        console.log(response);
-        var keys = Object.keys(response.product);
-        keys.sort();
-        var fields = [];
-        $.each(keys, function (index, k) {
-            fields.push(k + " (" + response.product[k] + ")")
+    var modelName = e.data.name;
+    var modelFields = e.data.fields;
+
+    var keys = Object.keys(modelFields);
+    keys.sort();
+    var fields = [];
+    $.each(keys, function (index, k) {
+        fields.push(k + " (" + e.data.fields[k] + ")")
+    });
+
+    getTemplate("formModel", function (tpl) {
+        //var fieldsControl = $(tpl.render({"fields": fields}));
+        var fieldsControl = $(tpl.render({"model": modelName}));
+        fieldsControl.find('button').on('click', function (e) {
+            e.target.closest('div.panel').remove();
+        });
+        $('#' + modelName + 'List').prepend(fieldsControl);
+
+        var e = {
+            "target": fieldsControl.find('div.panel-body'),
+            "data": { "fields": fields},
+        };
+
+        var panelBody = fieldsControl.find('div.panel-body');
+
+        getTemplate("field", function (tpl) {
+            var field = $(tpl.render({"fields": e.data.fields}));
+            panelBody.append(field);
+            field.fadeIn('slow');
+
+            // We do some tomfoolery here for UX purposes
+            // It breaks a million principles, but it's usable
+            var button = fieldsControl.find('button.btn-danger');
+            button.removeClass('btn-danger');
+            button.addClass('btn-success');
+
+            var span = button.find('span');
+            span.removeClass('glyphicon-minus-sign');
+            span.addClass('glyphicon-plus-sign');
+
+            button.on('click', e.data, addField);
+
         });
 
-        getTemplate("product", function (tpl) {
-            var product = $(tpl.render({"fields": fields}));
-            product.find('button').on('click', function (e) {
-                e.target.closest('div.panel').remove();
-            });
-            $('#productList').prepend(product);
-
-            var e = {
-                "target": product.find('div.panel-body'),
-                "data": { "fields": fields},
-            };
-
-            var panelBody = product.find('div.panel-body');
-
-            getTemplate("field", function (tpl) {
-                var field = $(tpl.render({"fields": e.data.fields}));
-                panelBody.append(field);
-                field.fadeIn('slow');
-
-                // We do some tomfoolery here for UX purposes
-                // It breaks a million principles, but it's usable
-                var button = product.find('button.btn-danger');
-                button.removeClass('btn-danger');
-                button.addClass('btn-success');
-
-                var span = button.find('span');
-                span.removeClass('glyphicon-minus-sign');
-                span.addClass('glyphicon-plus-sign');
-
-                button.on('click', e.data, addField);
-
-            });
-
-
-        }); // product
-    }); // getProductModel
+    }); // model
 }
 
 function addField (e) {
@@ -63,22 +63,32 @@ function addField (e) {
 }
 
 function submitForm (e) {
-    requestObject = {"product": []};
-    $('div.product').each(function (productPanel) {
-        product = {};
-        $(this).children('form').each(function (fieldForm) {
-            var fieldName = $(this).find('select.fieldName').find(':selected').text();
-            fieldName = fieldName.replace(/ \(.*/, '');
-            var fieldOperator = $(this).find('select.fieldOperator').find(':selected').text();
-            var fieldValue = $(this).find('input.fieldValue').val();
-            if (fieldValue != "") {
-                product[fieldName] = fieldOperator + fieldValue;
+    console.log(e.data);
+
+    requestObject = {};
+
+    var len = e.data.models.length;
+    for (var i = 0; i < len ; i++) {
+        var modelName = e.data.models[i];
+        console.log(modelName);
+
+        requestObject[modelName] = [];
+        $('div.' + modelName).each(function (modelPanel) {
+            model = {};
+            $(this).children('form').each(function (fieldForm) {
+                var fieldName = $(this).find('select.fieldName').find(':selected').text();
+                fieldName = fieldName.replace(/ \(.*/, '');
+                var fieldOperator = $(this).find('select.fieldOperator').find(':selected').text();
+                var fieldValue = $(this).find('input.fieldValue').val();
+                if (fieldValue != "") {
+                    model[fieldName] = fieldOperator + fieldValue;
+                }
+            });
+            if (!$.isEmptyObject(model)) {
+                requestObject[modelName].push(model);
             }
         });
-        if (!$.isEmptyObject(product)) {
-            requestObject["product"].push(product);
-        }
-    });
+    }
 
     var promise = findUser(requestObject);
 
@@ -116,8 +126,23 @@ $(window).ready(function () {
         var body = $(tpl.render({}));
         body.fadeIn('slow');
         $('#content').prepend(body);
-        $('#addProduct').on('click', addProduct);
-        $('#submit').on('click', submitForm);
+
+        getDataModel().success(function (response) {
+            for (var key in response) {
+                var root = $('div.panel-body');
+                getTemplate("addModel", function (tpl) {
+                    var model = $(tpl.render({"model": key}));
+                    model.fadeIn('slow');
+                    root.append(model);
+                    $('#add' + key).on('click', { name: key, fields: response[key] }, addModel);
+                });
+            }
+
+            var models = Object.keys(response);
+            $('#submit').on('click', {models: models}, submitForm);
+
+        });
+
         getTemplate("status", function (tpl) {
             var status = $(tpl.render({}));
             status.fadeIn('slow');
